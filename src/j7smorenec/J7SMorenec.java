abd71c705837993fa1475593eb41e4b16617379b
sqr <sqr@sqr-1101HA>
2015-07-19 17:52:10 +0300

    Task3 is done.

--- src/j7smorenec/J7SMorenec.java
+++ src/j7smorenec/J7SMorenec.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 /**
  * Java 7 SE training
+ *
  * @author vgalunov
  */
 public class J7SMorenec {
 
     /**
      * Return tasks numbers using first letter of student's last name
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -17,18 +19,18 @@ public class J7SMorenec {
         Scanner sc = new Scanner(System.in);
         String lastName = sc.next();
         char firstLetter = lastName.charAt(0);
-        int rest = firstLetter % 4;        
-        System.out.print("Tasks for "+lastName+": ");
-        if (rest==0){
-            rest+=4;
+        int rest = firstLetter % 4;
+        System.out.print("Tasks for " + lastName + ": ");
+        if (rest == 0) {
+            rest += 4;
         }
-        for(int i=rest; i<=56; i+=4){
+        for (int i = rest; i <= 56; i += 4) {
             System.out.print(i);
-            if (i<53) {
+            if (i < 53) {
                 System.out.print(", ");
             }
         }
         System.out.println(".");
     }
-    
+
 }
---
+++ src/j7smorenec/Task3.java
@@ -0,0 +1,22 @@
+package j7smorenec;
+
+/**
+ * Task3. Given 3 variables - A,B,C. Replace their values - A to C, C to B, B to
+ * A.
+ *
+ * @author sqr
+ */
+public class Task3 {
+
+    public static void main(String[] args) {
+        int A, B, C;
+        A = 64;
+        B = 512;
+        C = 128;
+        int d = A;
+        A = B;
+        B = C;
+        C = d;
+        System.out.println("A=" + A + " B=" + B + " C=" + C);
+    }
+}
