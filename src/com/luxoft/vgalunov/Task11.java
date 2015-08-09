9bfea3f8096cabaf05b43130f9c1b4c5463cde8c
sqr <sqr@sqr-1101HA>
2015-08-09 16:52:32 +0300

    Task11 done.

---
+++ src/com/luxoft/vgalunov/Task11.java
@@ -0,0 +1,15 @@
+package com.luxoft.vgalunov;
+
+/**
+ * Task11. Given double-digit number. Swap digits without any additional
+ * variables.
+ *
+ * @author vgalunov
+ */
+public class Task11 {
+
+    public static void main(String[] args) {
+        int ddnumber = 64;
+        System.out.println("Swaped digits number:" + String.valueOf(ddnumber).charAt(1) + String.valueOf(ddnumber).charAt(0));
+    }
+}
